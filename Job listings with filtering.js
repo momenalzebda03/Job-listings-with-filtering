a1432c7f232f5e1a8b0424ccc5e69101977934f3
unknown <momen07996@gmail.com>
2022-06-19 05:40:31 -0700

    Choose a programming language

---
+++ Job listings with filtering.js
@@ -0,0 +1,176 @@
+var click = function () {
+
+var click = document.getElementById("p14");
+
+var click1 = document.getElementById("img2");
+
+click.style.display = "inline-block";
+click1.style.display = "inline-block";
+}
+
+document.getElementById("p9").onclick = click;
+
+var click1 = function () {
+
+var click = document.getElementById("p14");
+
+var click1 = document.getElementById("img2");
+
+click.style.display = "none";
+
+click1.style.display = "none";
+}
+
+document.getElementById("img2").onclick = click1;
+
+var click2 = function () {
+
+var click = document.getElementById("p15");
+
+var click1 = document.getElementById("img3");
+
+click.style.display = "inline-block";
+click1.style.display = "inline-block";
+}
+
+document.getElementById("p10").onclick = click2;
+
+var click3 = function () {
+
+var click = document.getElementById("p15");
+
+var click1 = document.getElementById("img3");
+
+click.style.display = "none";
+
+click1.style.display = "none";
+}
+
+document.getElementById("img3").onclick = click3;
+
+var click4 = function () {
+
+var click = document.getElementById("p16");
+
+var click1 = document.getElementById("img4");
+
+click.style.display = "inline-block";
+click1.style.display = "inline-block";
+}
+
+document.getElementById("p11").onclick = click4;
+
+var click5 = function () {
+
+var click = document.getElementById("p16");
+
+var click1 = document.getElementById("img4");
+
+click.style.display = "none";
+
+click1.style.display = "none";
+}
+
+document.getElementById("img4").onclick = click5;
+
+var click6 = function () {
+
+var click = document.getElementById("p17");
+
+var click1 = document.getElementById("img5");
+
+click.style.display = "inline-block";
+click1.style.display = "inline-block";
+}
+
+document.getElementById("p12").onclick = click6;
+
+var click7 = function () {
+
+var click = document.getElementById("p17");
+
+var click1 = document.getElementById("img5");
+
+click.style.display = "none";
+
+click1.style.display = "none";
+}
+
+document.getElementById("img5").onclick = click7;
+
+var click8 = function () {
+
+var click = document.getElementById("p18");
+
+var click1 = document.getElementById("img6");
+
+click.style.display = "inline-block";
+click1.style.display = "inline-block";
+}
+
+document.getElementById("p13").onclick = click8;
+
+var click9 = function () {
+
+var click = document.getElementById("p18");
+
+var click1 = document.getElementById("img6");
+
+click.style.display = "none";
+
+click1.style.display = "none";
+}
+
+document.getElementById("img6").onclick = click9;
+
+var click10 = function() {
+
+var click = document.getElementById("p14");
+
+var click1 = document.getElementById("img2");
+
+var click2 = document.getElementById("p15");
+
+var click3 = document.getElementById("img3");
+
+var click4 = document.getElementById("p16");
+
+var click5 = document.getElementById("img4");
+
+var click6 = document.getElementById("p17");
+
+var click7 = document.getElementById("img5");
+
+var click8 = document.getElementById("p18");
+
+var click9 = document.getElementById("img6");
+
+click.style.display = "none";
+
+click1.style.display = "none";
+
+click2.style.display = "none";
+
+click3.style.display = "none";
+
+click4.style.display = "none";
+
+click5.style.display = "none";
+
+click6.style.display = "none";
+
+click7.style.display = "none";
+
+click8.style.display = "none";
+
+click9.style.display = "none";
+}
+
+document.getElementById("p19").onclick = click10;
+
+var click11 = function () {
+
+prompt("are you sure");
+}
+
+document.getElementById("p20").onclick = click11;
